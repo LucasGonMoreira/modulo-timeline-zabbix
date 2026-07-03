7bb16c1f35ef73f9bc0bf94df8226b9d537c2c2a
Lucas Gonçalves Moreira <143469314+LucasGonMoreira@users.noreply.github.com>
2026-07-02 22:55:18 -0300

    Add files via upload

--- install_zabbix_timeline.sh
+++ install_zabbix_timeline.sh
@@ -8,91 +8,106 @@ echo ""
 
 # Find Zabbix UI Directory
 ZABBIX_UI_DIR=""
-if [ -d "/usr/share/zabbix/ui/modules" ]; then
-    ZABBIX_UI_DIR="/usr/share/zabbix/ui"
-elif [ -d "/usr/share/zabbix/modules" ]; then
-    ZABBIX_UI_DIR="/usr/share/zabbix"
-else
-    echo "Erro: Diretório do Zabbix não encontrado. Este script suporta /usr/share/zabbix ou /usr/share/zabbix/ui."
-    exit 1
+POSSIBLE_DIRS=(
+    "/usr/share/zabbix/ui"
+    "/usr/share/zabbix"
+    "/var/www/html/zabbix"
+    "/var/www/zabbix"
+    "/usr/share/nginx/html/zabbix"
+)
+
+for dir in "${POSSIBLE_DIRS[@]}"; do
+    if [ -d "$dir/modules" ]; then
+        ZABBIX_UI_DIR="$dir"
+        break
+    fi
+done
+
+if [ -z "$ZABBIX_UI_DIR" ]; then
+    echo "Não foi possível detectar automaticamente o diretório do Zabbix UI."
+    read -p "Digite o caminho absoluto do Zabbix (ex: /usr/share/zabbix): " ZABBIX_UI_DIR
+    if [ ! -d "$ZABBIX_UI_DIR/modules" ]; then
+        echo "Erro: O diretório '$ZABBIX_UI_DIR/modules' não existe."
+        exit 1
+    fi
 fi
 
-echo "Diretório do Zabbix encontrado em: $ZABBIX_UI_DIR"
+echo "Diretório do Zabbix encontrado/definido em: $ZABBIX_UI_DIR"
 MODULES_DIR="$ZABBIX_UI_DIR/modules"
 
 # Decode and extract module payload
 echo "Extraindo arquivos do módulo..."
 PAYLOAD=$(cat << 'EOF'
-H4sIAAAAAAAAA+0ca2/bRjKf8yu2QlBSOVmRZMsGlHNyssTEAizLkOQ0aSIQlLi2WFOkSlJ+tPWP
-OdyH4j7cp8P9Av+xm32QXL70SG03TThAEGp3Z3Y4Mzs7M7u0Z8ywaVj4xZOHgwrAXr1O/6/sVKP/
-M3hSrddq1d1KfacC7dXaXrXyBNUfkKcAFq6nOQg9ubq62tI1T8sat6r/Lwqer/9LA1+5D2MF6+u/
-Wqnv1kD/O5V6Ndf/Y0BM//7PMtYNrzyfzu9jjhX6393Zqwf636ttg/73trdrT1DlPiZfBd+4/v/+
-GpT89MXz5+gflyCH1jswA/TMmxouev4ibNccR7tBz4gASPvTZ2e2M0P7SLZgeOsN/JClue16UglJ
-v2jjsXFNjEcqFp8igK1XLvY6uiz55qUS81IJDUkYcazNcPqYl3zGrVearr/THFnSJp5hW2S6wGRd
-7RKTocYZkimnH4M+Q5dG6Lt9VCmiX+l8MWrCuBJKwQWqt5wF1TRcL/LmR9AgB28KJPv2lUx/EWDj
-jrQxNmVVloZ3//EWpi0VKeeeCRwX6cs3XQ87hnvR1ZwLuViK4Q/xtXdgX8scR2CS/ByV0JlmuriE
-avU658QHSvwHQ/em8o8H79Wh8n7Y7CtNdTBsHreb/bb6Q6c9PEzBaXqeY4wXHqhEW3j2mT1ZuETe
-4Y/lSA7+eWE4mEg0fGYoa8qqjd2JY9z9fvcvJi+d/p5Txa8ttaaDNTmCGghPbBw9gNjuQQLA54tD
-29GQ/AFgq9vdarfR4WGj220MBkUqFcOaGDq2PJUY7IbWFMUtIZAL8P1ha7alo8OG0XBDYUWHgri+
-RHkNPM0jhgkEXP6YePkBNvHEk8MBSZ7eaeYC+z6EjxvFxwErPWo7rsxpNhoTMDYP8+Y3jj1rEr8p
-f4ygEqig/VcIGG6OseMR606MqPIRHesSu55xrll66rgaH9fHrm1eGmRQZEyqYb8h61cbm1gx8QyU
-SnyzL49N5A2TzuFN7/55iU0qdYc1GOONHZuIGRid2PjQTm6Z9UX2n46HZ3K4F9BOe+HNF2RbYKqW
-pljTsSNx3bSxp5lT7CLdRh2+jrCvTEm3J+rCMelgSeKNY1u/oS18Xs8eAKfWuS9BCZj2QDR0TGhf
-UUvj2wPnYqCZsJXHbUiamJrLyEi6oZn2+QJvXRn6Ofa26I4aG36B8bw3xxbF8JwFjvUb7mAxnhle
-Vj/fuOl80a1+bs8X87JLseXiSynAG9GnEX9x5rNVwyKYjE4wkoQqKURBL79GuGCTNNDZwqLsyMXY
-AJ8YD3NARQuyTsogFb5kDm5g0RSSwUoB7CGNEmgYCFGbh0e5EEZJWRiEGtkAONob/lMm7SkoiYYz
-7E2mMkxGuD6FOWGBJt+SwAx7U1tvoMJJbzAsJH0MAWKQjYCnxJDbYqKp7E2xJfMljImi/OfyTy6R
-eRYKjTJheDqzJLwjI8BSJhPsummq88G+xI6p3bS5XUNA4Tn2jZwiPR9Me6IRiyg72LQ1PWvoLcLg
-ipbMrJng2OWC4jg20mzk0rWHbORvo/i7NLVT0mvJFrgE5YLdZspps3df471vY20ho7esQ3o6Al9I
-9EPTBfUC36j4GlykK0sLF9wh9+tF9P33voen7SMSi40X5+rM1sHRo/199LbfOz1R28rB6Vu122sr
-qnLcPDhS2r62WyeOfWaAkBsNsO6OBbuXNcEy2XI8e+6zz/0yJ08oL0GcaRf0/9DV0qgfT6Y2Igar
-YmsC/MmcKPT+2cnbPUBW/k92tkfK/3cqlZqf/1drlQrN/2t7ef7/GPC5+T+LEII8+NCbmSfaOZbF
-jH9Iog+a9HKrQjoO4x83Uh1o2Ra4J9OVeVioncuSpV2Cy4jGEDwAFVJuH8LYLOG5eOBvzOYmPqBx
-E+Gq5RjwZkI8lrIhMcK21TKNyYUs/aRdaiz+aKATe346DyMAWjErwA57C//8QEqlwRVsrODYNHOL
-BiNb59iCgHhSAH8ai9V5tFlM5N/A6ZZJgm6QB+EchAUsowkXGmH9ZWpRhtOKVVbCoopHkoAAZUh+
-dawzO6LGQxrLCkkMyUraYhwpZqqx9uHdv52ZYdmJZr8MIrYG6ZvYGMsxxK7m3e93/8N+zjKib8TS
-GHWmzYNonCZb3AbmGtW9n3cR+YDZtIiaaOowGH44UtQ+bDRspmoSN5qRZVDo9ZvHbxVOpJYkIqRr
-GRTe9hXluEi3VNAX1ibToKzlBxGQlSLNRc/830F1i0ajY88KNHtkWBdkWgU6aA6ARFu+tA1drhBz
-DO1xQ6sPq2UNJJUDjqJltLK02eLgm7gO2bWHU9+nTbr+0AuRcAXW0ZkBq6UglYlt4hmagHngXzT0
-84I4LRf/pCGdTYVA9zgM415LxbJUKILkyWp0vSVx/muW+uwHsmPvBXEgQSqTJe+c0zQjlK+hg3CX
-yBNwU8P8aER/W2SBNQsXE6EeELml7oDZjqXTGo8K8SKRtzC33wVm94rUUl+jX8bXKika1WCw3G4O
-FXXY6Srqm16/2xyqA6XVO24PSiiVRhGBqWxJ/rzUFQUVh9DbZLx76Ag25yFeywppRd6+lM4Dq7iG
-faHH+SiM88tGo1Qq0cJGMILtpbZD97ePwTIuiWtgFHq722RZgkoRuvgGHStYiB3uFMT8dQSxOXw2
-BPF/19YXJr6vkD8Cq85/d2t+/E+G7ZLz393d3Tz+fwxg8f9TCwJEd65NMGJ24H7yY3Zwz5Cjox/p
-NvapBZHIpxYbw9xW8+SEPbS62FoQVwMYdGfnpBC+hmBVdxFHgxiFefz5YgzbcVCDgz2V1P0aiGze
-Qk0DJmg0WvYM/CXZGiFYAvclS7BPLsozzbASx1BnhqX3nKaukwChawNVm6TzyUo/0CG1SiAkx7sM
-y4UIoHnmQeALVA5t16PHChmphf/qYrLDAjuyq7MSoxTJreORPwt1bsGlP67+g/U/0yzjDGIbWpu7
-3zlWrP9qrV4L73/skfs/9Z1aJV//jwFsnRV87auX2HHBWgsNVCtX2MIuQBAKkaRvKbw+XCAug7Sn
-J/f+qJBeoVquiLjU3YgE/E7hWJZ0d+/+C27DJuSDqeaaoyEHnxukpomA+YVmkgFBWO4iTE6Lpppb
-9smy6NsFkqFvKUTWZKSLdlM/JvJIArNYgbxAKmeigBitsDhaSpmOpk2rp1NodrViOpwyyNRu7AWZ
-gT/RVb2CJ3LaswZPAzJsOSWe2Kym1WYDQ2pPBZoFmpadEzKP7xe/FQj8P18eD3ED8DPu/23Xd/L7
-f48BCf37S/N0TnLqe8kHVtX/67XtqP5r0FrP9//HgET87+v/EwtbXR7/t3iB3sTOy3gDr9DiPuxE
-Dp54bED7IEgEojYVJgQhCZIUEJ8/d2wPKGB9ZVpAzyi2XukGvcfScp2zd5pp6Bo7zvfD6Syikyme
-XHSs+YKSHtu2KZI+M7AJ/O0nr1ME9Sd678Cl54TxGxLhpYvgHslvbORvlu2peDb3buI44lW0JaSj
-hau0KeIYvAwVHWpYqFKqlmrxwWJVSuQivIkRngA72Fs4lq+GSyZ7zETKBbieFk6wMzNcl16kSuoi
-Og2pcEJiNryZY7mIXu2j04HSV4cfThT1x+bBQee9ShpWTavbPCVLs6pAxaD+cFb2XtF7ohXhPPwZ
-VXo6Cr3kKQwVdJ2GEL2qKEVQI/oHZNCPZ5NnOUEmfi1QoMJsIm1ubi2xdxPMIg0penNLCirJBOhd
-XEGm4h3ckJ+fSclcOj0hxVtazo0wTyP+gTJEUjmRfjPx7ktlWgQekwowU0WxTHhJQRDEG0UTOjKR
-I3xF0SNdmQSYgKOYrC0TRRBvFE/oKKbgoR8Olb6CQuHHpeS3x651tA/wNZ6Qc0eiGKEz9c7LM8e+
-AuW1D9gxRPvA5dcrB8qR0hqibvO9LEyFmgNI167BEt70e910XUeMlc5h4SuVLkiZTkduj8D/HyVK
-iRwkvI42oL+hKmqgapRMqtF1jsFhDFHneNjLsDyB/RKitlVCgq2UUASlhJg6S0hUD3rXPDpVBkhO
-UbCgE/aeWZawrolvZtObmvDahruWpUpFaV3re5pwSNg/aMsORdh1gk8tci4W/TCBleb8A7e07wlY
-mS7i5KnnAzx/muDV8J9WwPuDkBn/kzT/nk4D2Ec+O5nf/9Th2b//s12j9392yPdfefz/8MDi/+zy
-f3YaUEKtvm3iQ2zOSU4QC/WJ+XwdgT4ACYUNy9tOBMw81Pfh80P+DXE/Pw9I43StdGAVh19MjkAR
-mvRqsCxYaKNx2lG7vePOsNfvHL9VT/q9gyOlO1jJwLJs4Q/E4+h1g8ZmcmpkLoO5FbPCcwEjRJ0s
-HIeQ9y8wAJGKQNk2dTWgXklE6HSiqeYm8xx2WXdJIlRMD+pXxIV+lBSwmx0Prgpkl7BWjIeS9G2B
-tbRL42kiZGFleGcleSs6Klk23r/7kXlfOiZ+H30f1eKM0X5hiu9SxlA2BKbjZkWnzLqwLiImJJD5
-AqmpQLrpiYGbr9s0j+sXTbIy6BUVk6V5dBw56T+jSzk2PNA/Hen/yq60cIWuqK4sTaTD4+HRwy/W
-MOhOppH3lZUnblCRvGGjBD2kEP1wM4POGrl68iZYBi1f5VlkhEttGRTSEv/kZbEs7MwaQMaNss+p
-ByxzoxFqK0sCQbq+SVVgw5JA+qVAWiLg05OVwB5BzX6VQGxYo0oQLozC/RcKAj+yvGRQSOoy6zpo
-oZQyePkaXB8ltuhSEbNX1srx4RJaOdRfLRswv2x1FIqFSJ3BfwoeEl8AAQvs27PgI8eRQAFfG95f
-phqQmf+zo/nHOP+rbm/vCPk/uf+zt0fOf/P8/+HhofJ/Zj5fRwUgliyTOsC3mO2Gm6HUhm0cgsIH
-ytmEgPdrdr1fBGT6/6P7+wB0lf+v79Ri93/2dvZy//8osPL+95INgC60lMMX8i0g71yyQxAD++L3
-B+5siZ/5K7vuIBJ2aVYWy8aeL3PkvX5b6aODD9HsBbWVQSt6q4H9pQrx40T6p0lGwg4ZPFxNDRPz
-09wwTQy5LCbKiYkPHz+OeLntc48I6d/SYH+PIHnXAad/wPz1HQdm+n/l/v4A4Ar/X6vtJc//tvPv
-/x8F/jz/Twzsi/f/a+cH325aEHj+pQKqZF+SXHkXcvXpJzvxyC7Ix2dPXHSMaG79cvump15Ld9rH
-Oubi+orunUz94dND7aQZfwrta9tWc8ghhxxyyCGHHHLIIYcccsghhxxyyCGHHHLIIYcccsghhxwe
-Hf4PdiAiOwB4AAA=
+H4sIAAAAAAAAA+0ca2/iSHI+z6/oRaO1mSMMkJBIzGVyBJhJpJBEQOaxM8gyuBO8MTZrmzx2Nz/m
+dB9W9+E+ne4X5I9dVbcf7ReQbJKdnXVJUUx3V3W5qrq6qrrB1afU0E366tnjQQVgq15n/ysb1eh/
+Ds+q9Vqtulmpb1SgvVrbqlaekfoj8hTA3HFVm5Bnl5eXa5rqqlnjlvX/ScH19X+h00vncaxgdf1X
+K/XNGuh/o1Kv5vp/Cojp3/9YpprulmeT2UPMsUT/mxtbG77+a/UajKtura+vPyOVh5h8GfzF9f/3
+HVDy81cvX5J/XIAcWu/BDMgLd6I75OWrsF21bfWavEABYPvzF6eWPSXbRDZheOstfJClmeW4UolI
+P6ujkX6FxiMVi88JwNobh7r7miz55qWgeSlIQxJGHKpTmj7mtTfj2htV096rtiypY1e3TJwuMFlH
+vaA4VD8lMuP0c9Cna9KQfLdNKkXyC5svRk0YVyIpuED1xmNBMXTHjbz5ATTIwZsCyZ51KbNPCHzc
+gTqihqzI0uD2P+7csKQi49w1gOMie/mm41Jbd867qn0uF0sx/AG9cnetK9nDEZjEj8MSOVUNh5ZI
+rV73OPGBEf+ga+5E/mH3ozLofBw0e52m0h80D9vNXlv5sN8e7KXgNF3X1kdzF1Sizl3r1BrPHZR3
++GExkk1/mus2RYmGzxxlRVm1qTO29dvfbv/F5aWxzzOm+JWl1rSpKkdQA+GJjcNHENsDSAD4fLVn
+2SqRPwGsdbtr7TbZ22t0u41+v8ikoptjXaOmq6DB3tGaorglAnIBvj+tTdc0stfQG04orOhQENfX
+KK++q7pomEDA8R4TL9+nBh27cjggydN71ZhT34d444bxccDKEbMdR/ZoNhpjMDaXes1vbWvaRL8p
+f46gIlTI9hsCDDdH1HbRuhMjqt6IffOCOq5+pppa6riaN65HHcu40HFQZEyqYb/F9auODNox6BSU
+ir7Zl8dd5A2TzuBNb/95QQ0mdZs36KM7OzYRMzA6sRGcnGvPH83HLTK+yPaz79KpHG4FrNOau7M5
+7gpc09KEqhq1JU81beqqxoQ6RLPIvreMqK9LSbPGytw22GBJ8hpHlnbNWrx5XasPnJpnvgAlYNoF
+ybAxoXlFDc3bHTwu+qoBO3nchKSxoTqcjKTpqmGdzenapa6dUXeNbaix4eeUzo5m1GQYTB/Rft3p
+z0dT3c3q9/ZtNl90p59Zs/ms7DBsufhaCvCG7GnovTh32YpuIianE4zESCWFKOjllwgXfJIGOZ2b
+jB25GBvgE/OiHFDRHJdJGaTirZjda1gzhWSsUgB7SKMEGgZCzOThUS6EQVIWBlJD/++hvfU+ytie
+gpJoOKXueCLDZMj1CcwJ6zP5lghT6k4srUEKx0f9QSHpYhDQIBsBT4khN8VEU9mdUFP2VjBFRfnP
+5R8dlHkWCgsyYXg6sxjd4QiwlPGYOk6a6nywLqhtqNdtz64hnnBt61pOkZ4PhjVW0SLKNjUsVcsa
+ekMohFsLZlYN8OtyoWPbFlEt4rC1Ryzi76L0uzS1M9IryRa4BOWC3WbK6W7vvsJ738TaQkZveIf0
+fAi+EPXDsgXlnF4r9ApcpCNLcwfcoefWi+T7730Hz9qHGIqN5mfK1NLAz5PtbfKud3RyrLQ7uyfv
+lO5Ru6N0Dpu7B522r+3WsW2d6iDkRgOse9+EzcscUxl3HNea+ex7ftkjj5QXIE7Vc/Y/dLUs6Kfj
+iUXQYBVqjoE/2SMKvX907vYQkJX/49b2RPn/RqVS8/P/aq1SYfl/bSvP/58C7pv/8xAhyIP33Klx
+rJ5RWcz4Bxh+sKTXsyqi0TAAciLVgZZlgn8yHNkLC9UzWTLVC6kUCyK8AFRIuX0Ig7OE6/ICf306
+M+guC5yQq5atw5sJAVnKjsQJW2bL0MfnsvSjeqHyAKRBjq3ZySwMAVjFrABb7A38+ZGUwqIr2FnB
+s6nGGotG1s6oCQHxuAAONRare+FmMZF/A6drBgbdIA/kHIQFLJOxJzRk/XVqUcajFaushEUVF5OA
+AGWAn/bNUyuixj0WzApJDGYlbTGQFDPVWPvg9t/2VDetRLNfBhFbg/RNbIzlGGJX8/a32/9RP2cZ
+sjfiaYwyVWdBOM6SLc8GZirTvZ93oXzAbFqoJpY79AefDjpKD3YaPlM1iRvNyDIoHPWah+86HpFa
+koiQrmVQeNfrdA6LbE8FfVF1PAnKWn4UAVkpUR3ywv8cVLdYODpyzUCzB7p5jtN2oIMlAUS05QtL
+1+QKmmNoj3e0+rBa1iBSOeAoWkYrS3dbHN4urkF27dLU92lj1+96IYxXYB2d6rBaClIZbZNOyRjM
+g/6skp/m6LQc+qNKND4VAd3TMI7bkYplqVAEyeNqdNwFgf4Oz322A9nx94JAEJHKuOTtM5ZnhPLV
+NRDuAnkCbmqcHw3pb4o8subxYiLWAyI3zB1w2zE1VuNRIGBEeQtz+11gdm+wlrpDfh5dKVg0qsFg
+ud0cdJTBfrejvD3qdZsDpd9pHR22+yWSSqNIwFTWJH9e5oqCikPobTLePXQEd+chXssKaUXevpTO
+A6+4hn2hx/ksjPPLRsNUKtHCRjCC76WWzfa3z8EyLolrYBh6u5tkXYJJEbq8DTpWsRA7nAmI+RuJ
+YnO4LwTxf9fS5gZ9qJA/AsvOfzdrfvyPwzbx/HdzczOP/58CePz/3IQA0ZmpY0q4HThf/Jgd3DMk
+6eQHto19aUEk8qXFx3C31Tw+5g+tLjXn6GoAg+3sHilCryBY1RzioUGMwj3+bD6C7TgowsGeioW/
+BsHNWyhqwASNRsuagr/ErRGCJXBfsgT75Lw8VXUzcQx1qpvakd3UNAwQuhZQtTCfT1b6gQ4WK4GQ
+HO/STQcigOapC4EvUNmzHJcdK2SkFv6ri8kOD+xwV+c1RimSW8cjfx7q3IBLf1r9B+t/qpr6KcQ2
+rDj3sHMsWf946B/e/9jC+z/1jVolX/9PAXydFXztKxfUdsBaCw1SK1f4wi5AEAqRpG8pXoG4gC4D
+29OTe39USK9QLVdEXOZuRAJ+p3Asi93d2/+C27CQfDDVTLVVYtMzHYuaBJifqwYOCMJyh1A8Ep+o
+Ttkny6NvB0iGvqUQWZORLtbN/JjIIwZmsQp5AStnooA4rbA6WkqZjqVNy6frsOxqyXQ0ZZChXltz
+nMF7Yqt6CU943LMCT30ctpiSl9gsp9XmA0NqzwWaBZaWnSGZp/eLfxUI/L+3PB7jBuA97v+t1zfy
++39PAQn9+0vzZIY59YPkA8vq//XaelT/NWit5/v/U0Ai/vf1/4WHrY4X/7e8Ar1B7dfxBq9CS3uw
+E9l07PIB7d0gEYjaVJgQhCQwKUCfP7MtFyhQbWlawM4o1t5oOrvH0nLs0/eqoWsqP8/3w+ksouMJ
+HZ/vm7M5Iz2yLEMkfapTA/jbTt6nCOpP7OKBww4K41ckwlsXwUWSX/nIX03LVeh05l7HccSraAtI
+RwtXaVPEMbwyVHSobpJKqVqqxQeLVSmRi/AqRngEbFN3bpu+Gi647CkXqSfA1bRwTO2p7jjsIlVS
+F9FpsMIJidngekblInmzTU76nZ4y+HTcUX5o7u7uf1SwYdm0muWlZGlWFagY1B/Oyt8rek+0IhyI
+v2BKT0dhlzyFoYKu0xCiVxWlCGpE/4AM+nEtfJYTZOLXAgUq3CbS5vasJfZuglmkIUVvbklBJRmB
+3cUVZCrewQ35+QlL5tLJMRZvWTk3wjyL+PudAZHKifSbi3dbKrMi8AgrwFwVxTLykoIgiDeKJnRk
+Ikf4iqJHujIJcAFHMXlbJoog3iie0FFMwSMf9jq9DgmFH5eS3x6719HepVd0jOeOqBihM/XSywvb
+ugTltXf5MUR71/GuV/Y7B53WgHSbH2VhKtLsQ7p2BZbwtnfUTdd1xFjZHCa9VNiClNl0eH0E/n+W
+GCU8SNiJNpC/kSppkGqUTKrR7R+CwxiQ/cPBUYblCeyXCLOtEhFspUQiKCXC1VkionrI++bBSadP
+5BQFCzrh75llCaua+N1s+q4mvLLhrmSpUlFa1fqeJxwS9Q/askMRfp3gSwvPxaJfTOClOf/ALe37
+BLxMF3HyzPMBnj9N8Gr0Dyvg/U7IjP8xzX+g0wD+JZ+NzO//1OHZv/+zXmP3fzbw+195/P/4wOP/
+7PJ/dhpQIq2eZdA9aswwJ4iF+mg+30agD4ChsG6664mA2Qv1fbh/yH9H3PvnAWmcrpQOLOPwq8kR
+GEKT3Q2WBQttNE72le7R4f7gqLd/+E457h3tHnS6/aUMLMoWfkc8TnYaLDaTUyNzGcytmBWeCxgh
+6nhu20jev8AARCoCZcvQlIB6JRGhs4kmqpPMc/ht3QWJUDE9qF8SF/pRUsBudjy4LJBdwFoxHkqy
+twXW0m6Np4mQh5XhnZXkteioZPl4/+5H5oXpmPh99G1SizPG+oUpvksZw9gQmI6bFZsy68a6iJiQ
+QOYLpKYC6aYnBm6+btM8rl80ycqgl1RMFubRceSk/4wu5djwQP9spP8pu9LiKXRJdWVhIh0eDw8f
+f7GGQXcyjXyorDxxgwrzhjsl6CGF6Bc3M+iskKsnb4Jl0PJVnkVGuNSWQSEt8U9eFsvCzqwBZNwo
+u089YJEbjVBbWhII0vW7VAXuWBJIvxTISgTe9LgS+COo2a8SiA0rVAnChVF4+EJB4EcWlwwKSV1m
+XQctlFIGL16Dq6PEFl0qYvbKWjo+XEJLh/qr5Q7ML1odhWIhUmfwn4KHxFeAgAX+5bPgW45DgQK9
+0t0/TTUgM//nR/NPcf5XXV/fEPJ/vP+ztYXnv3n+//jwWPk/N59vowIQS5axDvBXzHbDzVBqwzYO
+QeEj5WxCwPstu96vAjL9/8HDfQF0mf+vb9Ri93+28Cehcv//BLD0/veCDYAttJTDF/wuoNe5YIdA
+A/vq9wfP2aKf+TO77iASdlhWFsvGXi5y5Ee9dqdHdj9FsxfS7vRb0VsN/KcqxC8nst8mGQo7ZPBw
+OdEN6p3mhmliyGUxUU5MfPHx89Art933iJD9mAb/QYLkXQea/gXmb+84MNP/dx7uBwCX+P/a+uZm
++Pt/lS1+/pf7/yeBP87/o4F99f5/5fzgr5sWBJ5/oYAq2Zckl96FXH76yU88sgvy8dnjpfgvrQ90
+hDcLGw3hh2lwUUhDsrOT0g9qVB3eKUXUvnqt/q5HZgu36ac6I/OUHd14ue2ET4+1DWf8kNq3tifn
+kEMOOeSQQw455JBDDjnkkEMOOeSQQw455JBDDjnkkEMOOdwH/g97epXtAHgAAA==
 EOF
 )
 
@@ -115,34 +130,75 @@ echo " Configuração do Banco de Dados"
 echo "=========================================="
 echo "Precisamos criar a tabela 'zbx_incident_timeline' no banco de dados do Zabbix."
 
-read -p "Qual é a senha do usuário 'root' (ou 'zabbix') do banco de dados (aperte Enter se não houver ou se usar auth socket)? " DB_PASS
+read -p "Deseja configurar o banco de dados automaticamente agora? (s/n): " RUN_DB
+if [[ "$RUN_DB" == "s" || "$RUN_DB" == "S" ]]; then
+    echo "1) MySQL / MariaDB"
+    echo "2) PostgreSQL"
+    read -p "Qual é o seu banco de dados? (1 ou 2): " DB_TYPE
 
-if [ -z "$DB_PASS" ]; then
-    MYSQL_CMD="mysql -u root zabbix"
+    read -p "Host do banco de dados (ex: localhost): " DB_HOST
+    DB_HOST=${DB_HOST:-localhost}
+    read -p "Nome do banco de dados (ex: zabbix): " DB_NAME
+    DB_NAME=${DB_NAME:-zabbix}
+    read -p "Usuário do banco de dados (ex: zabbix ou root): " DB_USER
+    DB_USER=${DB_USER:-zabbix}
+    read -p "Senha do banco de dados (deixe em branco se não houver): " DB_PASS
+
+    if [ "$DB_TYPE" = "1" ]; then
+        SQL_QUERY="CREATE TABLE IF NOT EXISTS zbx_incident_timeline (
+            timelineid BIGINT UNSIGNED NOT NULL,
+            title VARCHAR(255) DEFAULT '' NOT NULL,
+            description TEXT NOT NULL,
+            incident_time BIGINT UNSIGNED NOT NULL,
+            end_time BIGINT UNSIGNED DEFAULT 0 NOT NULL,
+            status INT DEFAULT 0 NOT NULL,
+            responsible VARCHAR(255) DEFAULT '' NOT NULL,
+            PRIMARY KEY (timelineid)
+        );"
+        MYSQL_CMD="mysql -u \"$DB_USER\" -h \"$DB_HOST\""
+        if [ -n "$DB_PASS" ]; then
+            MYSQL_CMD="$MYSQL_CMD -p\"$DB_PASS\""
+        fi
+        MYSQL_CMD="$MYSQL_CMD \"$DB_NAME\""
+        
+        echo "Executando script SQL no MySQL/MariaDB..."
+        eval "$MYSQL_CMD -e \"\$SQL_QUERY\""
+        
+        if [ $? -eq 0 ]; then
+            echo "Tabela criada com sucesso!"
+        else
+            echo "Aviso: Houve um erro ao criar a tabela no MySQL."
+        fi
+    elif [ "$DB_TYPE" = "2" ]; then
+        SQL_QUERY="CREATE TABLE IF NOT EXISTS zbx_incident_timeline (
+            timelineid BIGINT NOT NULL,
+            title VARCHAR(255) DEFAULT '' NOT NULL,
+            description TEXT NOT NULL,
+            incident_time BIGINT NOT NULL,
+            end_time BIGINT DEFAULT 0 NOT NULL,
+            status INT DEFAULT 0 NOT NULL,
+            responsible VARCHAR(255) DEFAULT '' NOT NULL,
+            PRIMARY KEY (timelineid)
+        );"
+        export PGPASSWORD="$DB_PASS"
+        echo "Executando script SQL no PostgreSQL..."
+        psql -U "$DB_USER" -h "$DB_HOST" -d "$DB_NAME" -c "$SQL_QUERY"
+        
+        if [ $? -eq 0 ]; then
+            echo "Tabela criada com sucesso!"
+        else
+            echo "Aviso: Houve um erro ao criar a tabela no PostgreSQL."
+        fi
+    else
+        echo "Opção inválida."
+    fi
 else
-    MYSQL_CMD="mysql -u root -p\"$DB_PASS\" zabbix"
-fi
-
-SQL_QUERY="CREATE TABLE IF NOT EXISTS zbx_incident_timeline (
-    timelineid BIGINT UNSIGNED NOT NULL,
-    title VARCHAR(255) DEFAULT '' NOT NULL,
-    description TEXT NOT NULL,
-    incident_time BIGINT UNSIGNED NOT NULL,
-    end_time BIGINT UNSIGNED DEFAULT 0 NOT NULL,
-    status INT DEFAULT 0 NOT NULL,
-    responsible VARCHAR(255) DEFAULT '' NOT NULL,
-    PRIMARY KEY (timelineid)
-);"
-
-echo "Executando script SQL..."
-eval "$MYSQL_CMD -e \"\$SQL_QUERY\""
-
-if [ $? -eq 0 ]; then
-    echo "Tabela criada com sucesso!"
-else
-    echo "Aviso: Houve um erro ao criar a tabela. Talvez o banco não se chame 'zabbix' ou o usuário root requeira ajustes."
-    echo "Você pode criar a tabela manualmente:"
-    echo "$SQL_QUERY"
+    echo "Você optou por não criar a tabela automaticamente."
+    echo "Por favor, rode o seguinte comando no seu banco de dados MySQL:"
+    echo "CREATE TABLE IF NOT EXISTS zbx_incident_timeline ( timelineid BIGINT UNSIGNED NOT NULL, title VARCHAR(255) DEFAULT '' NOT NULL, description TEXT NOT NULL, incident_time BIGINT UNSIGNED NOT NULL, end_time BIGINT UNSIGNED DEFAULT 0 NOT NULL, status INT DEFAULT 0 NOT NULL, responsible VARCHAR(255) DEFAULT '' NOT NULL, PRIMARY KEY (timelineid) );"
+    echo ""
+    echo "Ou se for PostgreSQL:"
+    echo "CREATE TABLE IF NOT EXISTS zbx_incident_timeline ( timelineid BIGINT NOT NULL, title VARCHAR(255) DEFAULT '' NOT NULL, description TEXT NOT NULL, incident_time BIGINT NOT NULL, end_time BIGINT DEFAULT 0 NOT NULL, status INT DEFAULT 0 NOT NULL, responsible VARCHAR(255) DEFAULT '' NOT NULL, PRIMARY KEY (timelineid) );"
 fi
 
 echo ""
